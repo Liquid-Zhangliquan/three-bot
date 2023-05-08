8f78e3a1d7ddfb39ae463ea39e29d243d1ef7b23
Liquid-Zhangliquan <1063754495@qq.com>
2023-05-08 17:52:53 +0800

    feat:learn

---
+++ src/views/Learn/learn.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home-container">
+    <three-box @ready="sceneReady" />
+  </div>
+</template>
+
+<script>
+import * as THREE from 'three';
+import ThreeBox from '@/components/three-box';
+export default {
+  name: '',
+  components: { ThreeBox },
+  data() {
+    return {
+    };
+  },
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {
+    sceneReady(data) {
+    },
+  },
+  destroyed() {},
+};
+</script>
+
+<style lang="less">
+.home-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
